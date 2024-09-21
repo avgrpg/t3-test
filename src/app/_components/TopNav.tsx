187472079c602aa2b4eb126ada09457024bfa264
avgrpg <tsuiwaiwai2000@gmail.com>
2024-09-21 14:56:25 +0800

    uploadthing setup

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,14 +1,26 @@
+"use client";
+
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "../utils/uploadthing";
+import { useRouter } from "next/navigation";
 
 export const TopNav = () => {
+  const router = useRouter();
   return (
     <nav className="flex items-center justify-between p-4">
       <div className="flex items-center gap-4"></div>
-      <div className="flex items-center gap-4">
+      <div className="flex flex-row items-center gap-4">
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => {
+              console.log("onClientUploadComplete");
+              router.refresh()
+            }}
+          />
           <UserButton />
         </SignedIn>
       </div>
