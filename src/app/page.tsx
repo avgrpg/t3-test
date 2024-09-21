5a8033341324329d291d4e046b9fa95b8715e137
avgrpg <tsuiwaiwai2000@gmail.com>
2024-09-21 21:24:56 +0800

    init next images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-import Link from "next/link";
-import { db } from "~/server/db";
+import Image from "next/image";
 import { getImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
-
-const mockData = [
-  {
-    id: 1,
-    url: "https://utfs.io/f/59d457da-e809-4b79-b87b-5f7343cae065-g0lhcn.jpg",
-  },
-  {
-    id: 2,
-    url: "https://utfs.io/f/e33fedc9-9ba1-410c-b067-45b8231487d3-ed973w.jpg",
-  },
-];
 
 export default async function HomePage() {
   const data = await getImages();
@@ -22,8 +10,17 @@ export default async function HomePage() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="flex gap-4 p-8">
         {data.map((post) => (
-          <div key={post.id} className="flex flex-col gap-2">
-            <img src={post.url} alt={post.name} />
+          <div key={post.id} className="flex w-48 h-48 flex-col gap-2">
+            <Image
+              src={post.url}
+              alt={post.name}
+              style={{
+                objectFit: "contain",
+              }}
+              // fill
+              width={192}
+              height={192}
+            />
           </div>
         ))}
       </div>
